45561e8a7c0a7e282c375059af428af9f3843484
Khanh Nguyen <nguyenk3@outlook.com>
2022-10-09 14:43:39 +1100

    initial commit

---
+++ get_prices.py
@@ -0,0 +1,14 @@
+
+import sys
+from datetime import datetime
+from cryptocmd import CmcScraper
+
+def main(coins):
+    today = datetime.today().strftime('%d-%m-%Y')
+
+    for coin in coins:
+        scraper = CmcScraper(coin, "01-01-2022", today, False, True, "AUD")
+        scraper.export("csv")
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
